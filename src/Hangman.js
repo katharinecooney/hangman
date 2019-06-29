c83a9a68e37ecec860df3a04421d278d296c0cbd
Katharine Cooney <katharinecooney@gmail.com>
2019-06-29 10:53:26 -0500

    Import randomWord() to select random answer from words file

--- src/Hangman.js
+++ src/Hangman.js
@@ -7,6 +7,7 @@ import img3 from "./3.jpg";
 import img4 from "./4.jpg";
 import img5 from "./5.jpg";
 import img6 from "./6.jpg";
+import {randomWord} from './words';
 
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
@@ -20,7 +21,7 @@ class Hangman extends Component {
     this.state = { 
       nWrong: 0, 
       guessed: new Set(), 
-      answer: "apple" 
+      answer: randomWord()
     };
     this.handleGuess = this.handleGuess.bind(this);
   }
@@ -71,7 +72,7 @@ class Hangman extends Component {
 
         {this.state.nWrong < this.props.maxWrong ? 
         <p className='Hangman-btns'>{this.generateButtons()}</p>
-        : <h1>You lose!</h1>
+        : <h1>You lose! The answer was {this.state.answer}</h1>
         }        
       </div>
     );
